224a0848e25e06b83dfaef5489c85e8b0192ea33
Sankalp <sankalp.m.1929@gmail.com>
2022-03-12 18:28:59 +0530

    gunicorn location changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 RUN_PORT=${PORT:-8000}
 
-/usr/local/lib/python3.8/dist-packages/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${RUN_PORT}" app.main:app 
+/usr/local/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${RUN_PORT}" app.main:app 
